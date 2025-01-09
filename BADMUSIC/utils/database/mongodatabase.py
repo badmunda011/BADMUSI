2971100171b30144b07a8ddcba3ac041d7d73e1e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-01-10 00:26:56 +0530

    Update mongodatabase.py

--- BADMUSIC/utils/database/mongodatabase.py
+++ BADMUSIC/utils/database/mongodatabase.py
@@ -1,5 +1,3 @@
-
-
 from typing import Dict, List, Union
 
 from BADMUSIC.core.mongo import mongodb
@@ -177,6 +175,29 @@ async def remove_private_chat(chat_id: int):
     if not is_served:
         return
     return await privatedb.delete_one({"chat_id": chat_id})
+
+# Users
+
+
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
 
 
 # Auth Users DB
